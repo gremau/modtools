43f76144ae7b7969222f2ab2f6c94572cc9eefcb
Greg Maurer <gregmaurer@gmail.com>
2017-07-14 23:16:21 -0600

    utils module

---
+++ utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def trimdates_dict(d, stdt, enddt):
+    d_trim = {}
+    for k in d.keys():
+        d_trim[k] = d[k].loc[np.logical_and(
+            d[k].index > stdt, d[k].index < enddt)].copy()
+    return d_trim
