50e3d591eaea70a4612958f34d0852693275732c
Gregory Maurer <gregmaurer@gmail.com>
2017-11-28 13:05:55 -0700

    minor changes to fix pandas error

--- centuryio.py
+++ centuryio.py
@@ -134,7 +134,7 @@ def dcindex_ydoy_dt( df, startyr=None ) :
     elif startyr is not None:
         offset = df_c.time.iloc[0] - startyr
         df_c.time = df_c.time - offset
-        df_c.year = df_c.time.astype(int).astype(str)
+        df_c['year'] = df_c.time.astype(int).astype(str)
         df_c['doy'] = df_c.dayofyr.astype(str)
         df_c['ts'] = df_c.year + df_c.doy
         newidx = pd.to_datetime(df_c.ts.values, format='%Y%j')
@@ -184,7 +184,7 @@ def dcindex_ymo_dt( df, startyr=None ) :
     elif startyr is not None:
         offset = df_c.time.iloc[0] - startyr
         df_c.time = df_c.time - offset
-        df_c.year = df_c.time.astype(int).astype(str)
+        df_c['year'] = df_c.time.astype(int).astype(str)
         df_c['month'] = df_c.month.astype(str)
         df_c['ts'] = df_c.year + df_c.month
         newidx = pd.to_datetime(df_c.ts, format='%Y%m') + pd.offsets.MonthEnd(0)
@@ -224,7 +224,7 @@ def dcindex_y_dt( df, startyr=None ) :
     elif startyr is not None:
         offset = df_c.time.iloc[0] - startyr
         df_c.time = df_c.time - offset
-        df_c.year = df_c.time.astype(int).astype(str)
+        df_c['year'] = df_c.time.astype(int).astype(str)
         #df_c['month'] = df_c.month.astype(str)
         #df_c['ts'] = df_c.year + df_c.month
         newidx = pd.to_datetime(df_c.year, format='%Y') + pd.offsets.YearEnd(0)
